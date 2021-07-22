1a88f2f2cc5732bd5e5033f40d5da9fd2a8f4e25
Mohamed H. Mohamed <mohamed@bsmg.co>
2021-07-22 15:52:27 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['images.unsplash.com', 'res.cloudinary.com', 'a.espncdn.com', 'bloximages.newyork1.vip.townnews.com'],
+    domains: ['content.mlb.com', 'images.unsplash.com', 'res.cloudinary.com', 'a.espncdn.com', 'bloximages.newyork1.vip.townnews.com'],
   },
 };
