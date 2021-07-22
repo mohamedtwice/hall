c51583b54fb508e72fa20585222c0bbee55000e8
Mohamed H. Mohamed <mohamed@bsmg.co>
2021-07-22 13:56:55 -0500

    Delete src/pages/api/readings directory

--- src/pages/api/readings/index.js
+++
@@ -1,8 +0,0 @@
-import { getReadings } from '@/lib/notion';
-
-export default async function handler(req, res) {
-  const { cursor } = req.query;
-  const readings = await getReadings(cursor);
-
-  res.status(200).json(readings);
-}
