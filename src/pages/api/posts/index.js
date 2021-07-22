72ed99d685c2a9ed78edbda4a08767a75f795d30
Mohamed H. Mohamed <mohamed@bsmg.co>
2021-07-22 13:56:45 -0500

    Delete src/pages/api/posts directory

--- src/pages/api/posts/index.js
+++
@@ -1,8 +0,0 @@
-import { getPosts } from '@/lib/notion';
-
-export default async function handler(req, res) {
-  const { cursor } = req.query;
-  const posts = await getPosts(cursor);
-
-  res.status(200).json(posts);
-}
